c0fd65797aab4ca0d9a3b81c20556502c83b1f2c
ketrololz <bqnexxx@gmail.com>
2024-11-23 21:39:35 +0300

    feat: add burger menu func

---
+++ christmas-shop/scripts/burger.js
@@ -0,0 +1,22 @@
+const burgerButton = document.querySelector('.burger')
+const burgerIcon = document.querySelector('.burger__line')
+const menu = document.querySelector('.nav')
+const menuLinks = document.querySelectorAll('.nav__list .links')
+const wrapper = document.querySelector('.wrapper')
+
+burgerButton.addEventListener('click', () => {
+  burgerIcon.classList.toggle('active')
+  menu.classList.toggle('active')
+  console.log(menuLinks)
+})
+
+function closeMenu(width = 769) {
+  if (wrapper.clientWidth > width && menu.classList.contains('active')) {
+    menu.classList.remove('active')
+    burgerIcon.classList.remove('active')
+  }
+}
+
+window.addEventListener('resize', () => closeMenu())
+
+menuLinks.forEach((item) => item.addEventListener('click', () => closeMenu(1)))
