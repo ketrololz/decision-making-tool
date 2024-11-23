986cce23250c4dcd1ea4aeb69ce01877e131e8a6
ketrololz <bqnexxx@gmail.com>
2024-11-23 23:03:11 +0300

    fix: change burger menu properties

--- christmas-shop/scripts/burger.js
+++ christmas-shop/scripts/burger.js
@@ -3,17 +3,19 @@ const burgerIcon = document.querySelector(".burger__line");
 const menu = document.querySelector(".nav");
 const menuLinks = document.querySelectorAll(".nav__list .links");
 const wrapper = document.querySelector(".wrapper");
+const body = document.querySelector("body");
 
 burgerButton.addEventListener("click", () => {
   burgerIcon.classList.toggle("active");
   menu.classList.toggle("active");
-  console.log(menuLinks);
+  body.classList.toggle("no-scroll");
 });
 
 function closeMenu(width = 769) {
   if (wrapper.clientWidth > width && menu.classList.contains("active")) {
     menu.classList.remove("active");
     burgerIcon.classList.remove("active");
+    body.classList.remove("no-scroll");
   }
 }
 
