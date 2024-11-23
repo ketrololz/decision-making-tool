70d052370dce7b128d54efea5588e4cdd3a7b68b
ketrololz <bqnexxx@gmail.com>
2024-11-23 21:48:26 +0300

    refactor: apply prettier

--- christmas-shop/scripts/burger.js
+++ christmas-shop/scripts/burger.js
@@ -1,24 +1,28 @@
-const burgerButton = document.querySelector('.burger')
-const burgerIcon = document.querySelector('.burger__line')
-const menu = document.querySelector('.nav')
-const menuLinks = document.querySelectorAll('.nav__list .links')
-const wrapper = document.querySelector('.wrapper')
+const burgerButton = document.querySelector(".burger");
+const burgerIcon = document.querySelector(".burger__line");
+const menu = document.querySelector(".nav");
+const menuLinks = document.querySelectorAll(".nav__list .links");
+const wrapper = document.querySelector(".wrapper");
 
-burgerButton.addEventListener('click', () => {
-  burgerIcon.classList.toggle('active')
-  menu.classList.toggle('active')
-  console.log(menuLinks)
-})
+burgerButton.addEventListener("click", () => {
+  burgerIcon.classList.toggle("active");
+  menu.classList.toggle("active");
+  console.log(menuLinks);
+});
 
 function closeMenu(width = 769) {
-  if (wrapper.clientWidth > width && menu.classList.contains('active')) {
-    menu.classList.remove('active')
-    burgerIcon.classList.remove('active')
+  if (wrapper.clientWidth > width && menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    burgerIcon.classList.remove("active");
   }
 }
 
-window.addEventListener('resize', () => closeMenu())
+window.addEventListener("resize", () => closeMenu());
 
-menuLinks.forEach((item) => item.addEventListener('click', () => {
-  if(!item.classList.contains('active')) { closeMenu(1) }
-}))
+menuLinks.forEach((item) =>
+  item.addEventListener("click", () => {
+    if (!item.classList.contains("active")) {
+      closeMenu(1);
+    }
+  })
+);
