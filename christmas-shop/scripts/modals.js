afe67d94be6591747ec40a2f28927a7827465e12
ketrololz <bqnexxx@gmail.com>
2024-11-25 00:24:07 +0300

    feat: add modal window func

--- christmas-shop/scripts/modals.js
+++ christmas-shop/scripts/modals.js
@@ -1,18 +1,106 @@
-const giftCardsArr = document.querySelectorAll('.gift-card')
-const modalWindow = document.querySelector('.modal-cards')
-const closeButton = document.querySelector('.close-button')
+import giftsJson from '../gifts.json' with { type: 'json' };
 
-console.log(giftCardsArr)
+const giftCardsArr = document.querySelectorAll(".gift-card");
+const modalWindow = document.querySelector(".modal-cards");
+const closeButton = document.querySelector(".close-button");
 
 function openModal(card) {
-  modalWindow.classList.remove('hidden')
+  changeData(card);
+  modalWindow.classList.remove("hidden");
 }
 
 function closeModal() {
-  modalWindow.classList.add('hidden')
+  modalWindow.classList.add("hidden");
 }
 
-// giftCardsArr.forEach(item => item.addEventListener('click', () => openModal(item.childNodes[3].childNodes[3])))
-giftCardsArr.forEach(item => item.addEventListener('click', () => openModal(item)))
+function changeData(card) {
+  const currentCardObj = giftsJson.find(
+    (item) => item.name === getCardName(card)
+  );
+  const modalImg = document.getElementById("modal-image");
+  const modalType = document.getElementById("modal-type");
+  const modalName = document.getElementById("modal-name");
+  const modalDescription = document.getElementById("modal-description");
+  const modalLive = document.getElementById("modal-live");
+  const modalCreate = document.getElementById("modal-create");
+  const modalLove = document.getElementById("modal-love");
+  const modalDream = document.getElementById("modal-dream");
+  const modalLiveLevel = document.getElementById("modal-live-level");
 
-closeButton.addEventListener('click', () => closeModal())
+  switch (currentCardObj.category) {
+    case "For Work":
+      modalImg.classList.remove("for-health-image");
+      modalImg.classList.remove("for-harmony-image");
+      modalImg.classList.add("for-work-image");
+      break;
+    case "For Health":
+      modalImg.classList.remove("for-work-image");
+      modalImg.classList.remove("for-harmony-image");
+      modalImg.classList.add("for-health-image");
+      break;
+    case "For Harmony":
+      modalImg.classList.remove("for-health-image");
+      modalImg.classList.remove("for-work-image");
+      modalImg.classList.add("for-harmony-image");
+      break;
+  }
+
+  switch (currentCardObj.category) {
+    case "For Work":
+      modalType.classList.remove("for-health");
+      modalType.classList.remove("for-harmony");
+      modalType.classList.add("for-work");
+      modalType.innerHTML = "For Work";
+      break;
+    case "For Health":
+      modalType.classList.remove("for-work");
+      modalType.classList.remove("for-harmony");
+      modalType.classList.add("for-health");
+      modalType.innerHTML = "For Health";
+      break;
+    case "For Harmony":
+      modalType.classList.remove("for-health");
+      modalType.classList.remove("for-work");
+      modalType.classList.add("for-harmony");
+      modalType.innerHTML = "For Harmony";
+      break;
+  }
+
+  modalName.innerHTML = currentCardObj.name;
+  modalDescription.innerHTML = currentCardObj.description;
+  modalLive.innerHTML = currentCardObj.superpowers.live;
+  modalCreate.innerHTML = currentCardObj.superpowers.create;
+  modalLove.innerHTML = currentCardObj.superpowers.love;
+  modalDream.innerHTML = currentCardObj.superpowers.dream;
+  clearSnowflakes();
+  fillSnowflakes();
+}
+
+function fillSnowflakes() {
+  const stats = document.querySelectorAll(".stats-list__item");
+
+  stats.forEach((item) => {
+    const count = +item.children[1].innerHTML / 100;
+    const snowflakes = item.childNodes[5];
+
+    for (let i = 0; i < count; i++) {
+      snowflakes.children[i].classList.add("filled");
+    }
+  });
+}
+
+function clearSnowflakes() {
+  const filledSnowflakes = document.querySelectorAll(".filled");
+
+  filledSnowflakes.forEach((item) => item.classList.remove("filled"));
+}
+
+function getCardName(card) {
+  return card.querySelector("h3").innerHTML;
+}
+
+giftCardsArr.forEach((item) =>
+  item.addEventListener("click", () => openModal(item))
+);
+
+closeButton.addEventListener("click", () => closeModal());
