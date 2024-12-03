344d713f8fea8c6a9d389724848449835e66c4c1
ketrololz <bqnexxx@gmail.com>
2024-12-03 12:28:05 +0300

    fix: change time in timer

--- christmas-shop/scripts/timer.js
+++ christmas-shop/scripts/timer.js
@@ -1,4 +1,4 @@
-const newYear = new Date("2024-12-31T00:00:00.000Z");
+const newYear = new Date("2024-12-31T23:59:59.000Z");
 const daysBox = document.getElementById("days");
 const hoursBox = document.getElementById("hours");
 const minutesBox = document.getElementById("minutes");
