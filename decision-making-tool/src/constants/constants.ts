7d29d949d973d375ad31254c3f8d09807fee421d
ketrololz <bqnexxx@gmail.com>
2025-03-16 21:41:46 +0300

    fix: add sounds imports

--- decision-making-tool/src/constants/constants.ts
+++ decision-making-tool/src/constants/constants.ts
@@ -2,6 +2,10 @@ import type { Options } from '../pages/options.ts';
 import type { Wheel } from '../pages/wheel.ts';
 import type Router from '../router/router.ts';
 
+import spinSound from '../../src/assets/sounds/spin.mp3';
+import endSound from '../../src/assets/sounds/end.mp3';
+import startSound from '../../src/assets/sounds/start.mp3';
+
 export const DEFAULT_ID = 1;
 export const CIRCLE = Math.PI * 2;
 export const MAX_TITLE_LENGTH = 10;
@@ -18,21 +22,26 @@ export const ROUTES = [
   {
     path: '/',
     page: (router: Router): Promise<Options> =>
-      import('../pages/options.ts').then((module) => new module.Options(router)),
+      import('../pages/options.ts').then(
+        (module) => new module.Options(router),
+      ),
   },
   {
     path: '/options',
     page: (router: Router): Promise<Options> =>
-      import('../pages/options.ts').then((module) => new module.Options(router)),
+      import('../pages/options.ts').then(
+        (module) => new module.Options(router),
+      ),
   },
   {
     path: '/wheel',
-    page: (router: Router): Promise<Wheel> => import('../pages/wheel.ts').then((module) => new module.Wheel(router)),
+    page: (router: Router): Promise<Wheel> =>
+      import('../pages/wheel.ts').then((module) => new module.Wheel(router)),
   },
 ];
 
 export const AUDIO_PATHS = {
-  'spin': '../../src/assets/sounds/spin.mp3',
-  'end': '../../src/assets/sounds/end.mp3',
-  'baraban': '../../src/assets/sounds/baraban.mp3'
-}
+  spin: spinSound,
+  end: endSound,
+  baraban: startSound,
+};
